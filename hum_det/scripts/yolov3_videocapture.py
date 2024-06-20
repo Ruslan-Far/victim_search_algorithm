06ab472a635ac888919ace2d587b6bf8df4b4c97
Ruslan-Far <rfarhetdinov@mail.ru>
2024-06-20 17:24:07 +0300

    hum_det: added yolov3_videocapture.py (работает быстрее)

--- hum_det/scripts/yolov3_videocapture.py
+++ hum_det/scripts/yolov3_videocapture.py
@@ -1,15 +1,9 @@
 #!/usr/bin/env python3
 
 import cv2
-import rospy
-from sensor_msgs.msg import Image
-from cv_bridge import CvBridge
 import numpy as np
 import time
 
-ROS_NODE_NAME = "first_sub"
-# ROS_IMAGE_TOPIC = "/pylon_camera_node/image_raw"
-ROS_IMAGE_TOPIC = "/usb_cam/image_raw"
 HEIGHT = 1024
 WIDTH = 1280
 WINDOW_ORIG = "original"
@@ -29,12 +23,14 @@ net = cv2.dnn.readNetFromDarknet(config_path, weights_path)
 ln = net.getLayerNames()
 ln = [ln[int(i - 1)] for i in net.getUnconnectedOutLayers()]
 
-
-def image_callback(msg: Image, cv_bridge: CvBridge) -> None:
-	img_bgr = cv_bridge.imgmsg_to_cv2(msg)
-	img_rgb = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
+# получаем видео с камеры
+# video=cv2.VideoCapture("rtsp://192.168.0.99:554/av0_0")
+video=cv2.VideoCapture(0)
+# пока не нажата любая клавиша — выполняем цикл
+while cv2.waitKey(1)<0:
+	hasFrame, img_rgb = video.read()
 	img_rgb = cv2.resize(img_rgb, (WIDTH, HEIGHT))
-
+	
 	blob = cv2.dnn.blobFromImage(img_rgb, 1/255.0, (416, 416), swapRB=True, crop=False)
 	net.setInput(blob)
 	start = time.perf_counter()
@@ -103,21 +99,3 @@ def image_callback(msg: Image, cv_bridge: CvBridge) -> None:
 	# cv2.imshow(WINDOW_ORIG, img_rgb)
 	cv2.imshow(WINDOW_YOLOV3, img_rgb)
 	cv2.waitKey(1)
-
-
-def main() -> None:
-	rospy.init_node(ROS_NODE_NAME)
-	sample: Image = rospy.wait_for_message(ROS_IMAGE_TOPIC, Image, timeout = 3.0)
-	if sample is not None:
-		rospy.loginfo(f"Encoding: {sample.encoding}, Resolution: {sample.width, sample.height}")
-	cv_bridge: CvBridge = CvBridge()
-
-	rospy.Subscriber(ROS_IMAGE_TOPIC, Image, lambda msg: image_callback(msg, cv_bridge), queue_size = None)
-
-	rospy.spin()
-
-	cv2.destroyAllWindows()
-
-
-if __name__ == '__main__':
-	main()
