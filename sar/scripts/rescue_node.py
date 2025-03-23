de156165381e29552fa347dc1a65ea97bd535fd8
Ruslan-Far <rfarhetdinov@mail.ru>
2025-03-23 23:19:26 +0300

    - потестил вывод текущего pose робота в rescue_node

--- sar/scripts/rescue_node.py
+++ sar/scripts/rescue_node.py
@@ -6,8 +6,8 @@ from sar.srv import *
 import actionlib
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 import math
-# import tf
-# import tf.transformations
+import tf
+import tf.transformations
 import tf2_ros
 
 NODE_NAME = "rescue_node"
@@ -26,6 +26,8 @@ x = 0
 cx = 0
 distance = 0
 f = 0
+tf_buffer = tf2_ros.Buffer()
+count = 0 # нужен для первого возможного некорректного вызова tf_buffer.lookup_transform
 
 
 def get_victim_coordinates(robot_x, robot_y, theta, distance, alpha):
@@ -35,36 +37,31 @@ def get_victim_coordinates(robot_x, robot_y, theta, distance, alpha):
 
 
 def get_robot_pose():
-	# listener = tf.TransformListener()
-	# try:
-	# 	listener.waitForTransform("/map", "/base_link", rospy.Time(0), rospy.Duration(5))
-	# 	(trans, rot) = listener.lookupTransform("/map", "/base_link", rospy.Time(0))
-	# 	x, y = trans[0], trans[1]  # Позиция робота
-	# 	quaternion = rot           # Кватернион
-	# 	euler = tf.transformations.euler_from_quaternion(quaternion)
-	# 	theta = euler[2]  # Угол поворота (в радианах)
-	# 	return x, y, theta
-	# except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-	# 	rospy.logerr("ошибка при получении координат робота")
-	# 	return None
-	tf_buffer = tf2_ros.Buffer()
-	listener = tf2_ros.TransformListener(tf_buffer)
-	# Ожидаем трансформацию, если она не существует
-	try:
-		# tf_buffer.wait_for_transform('map', 'base_link', rospy.Time(0), rospy.Duration(1.0))
-		transform = tf_buffer.lookup_transform('map', 'base_link', rospy.Time(0), rospy.Duration(1))
-		rospy.loginfo("Transform found!")
-		rospy.loginfo(f"{transform[0]}, {transform[1]}")
-		return None
-	except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException) as e:
-		rospy.logerr("Transform not found: %s", str(e))
-		return None
+	global count
+
+	while True: # нужен для первого возможного некорректного вызова tf_buffer.lookup_transform
+		try:
+			trans = tf_buffer.lookup_transform("map", "base_link", rospy.Time(0), rospy.Duration(1))
+			rospy.loginfo("transform found!")
+			x, y = trans.transform.translation.x, trans.transform.translation.y
+			quaternion = trans.transform.rotation
+			euler = tf.transformations.euler_from_quaternion([quaternion.x, quaternion.y, quaternion.z, quaternion.w])
+			theta = euler[2]
+			rospy.loginfo(f"theta in degrees: {math.degrees(theta)}")
+			return x, y, theta
+		except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException) as e:
+			rospy.logerr("transform not found: %s", str(e))
+			if count == 0: # нужен для первого возможного некорректного вызова tf_buffer.lookup_transform
+				rospy.loginfo("repeat...")
+				count += 1
+				continue
+			return None
 
 
 def get_alpha(x, cx, distance, f):
 	horizontal_offset_x = (cx - x) * distance / f
 	alpha = math.atan(horizontal_offset_x / distance)
-	rospy.loginfo(f"alpha: {alpha}")
+	rospy.loginfo(f"alpha in degrees: {alpha}")
 	return math.radians(alpha)
 
 
@@ -75,17 +72,15 @@ def rescue():
 	global distance
 	global f
 
-	pose = get_robot_pose()
-	if pose:
-		rospy.loginfo(f"робот находится в координатах: x={pose[0]}, y={pose[1]}, theta={pose[2]}")
 	if not is_on:
 		return
 
 	pose = get_robot_pose()
 	if pose:
-		robot_x, robot_y, theta = pose # получаем текущие координаты робота
+		robot_x, robot_y, theta = pose
+		rospy.loginfo(f"робот находится в координатах: x={robot_x}, y={robot_y}, theta={theta}")
 		alpha = get_alpha(x, cx, distance, f)
-		rospy.loginfo(f"alpha in radians: {alpha}")
+		rospy.loginfo(f"alpha: {alpha}")
 		victim_x, victim_y = get_victim_coordinates(robot_x, robot_y, theta, distance, alpha)
 		rospy.loginfo(f"координаты пострадавшего: x={victim_x}, y={victim_y}")
 		timeout = rospy.Duration(180)
@@ -93,7 +88,9 @@ def rescue():
 		if x == -1: # если нода det_img_group_node сама выключила данный процесс, то ничего ждать не нужно
 			rospy.loginfo("rescue completed!")
 			return
+		rospy.loginfo("before victim rescue")
 		rospy.sleep(10) # время для спасения пострадавшего
+		rospy.loginfo("after victim rescue")
 	is_on = False
 	call_rescue_mode_switch_feedback(is_on)
 	rospy.loginfo("rescue completed!")
@@ -149,6 +146,7 @@ if __name__ == '__main__':
 
 		rescue_mode_switch_server = rospy.Service(RESCUE_MODE_SWITCH_SRV, RescueModeSwitch, handle_rescue_mode_switch)
 
+		tf_listener = tf2_ros.TransformListener(tf_buffer)
 		while True:
 			rescue()
 			rospy.sleep(1)
